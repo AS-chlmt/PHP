9e23392c427c486070d57ded6fbdee707121d8b0
AS-chlmt <75732735+AS-chlmt@users.noreply.github.com>
2020-12-16 18:14:56 +0300

    Release

--- php/32.php
+++ php/32.php
@@ -9,11 +9,39 @@
 		$_SESSION["to"] = $to;
 		$_SESSION["subject"] = $subject;
 		$_SESSION["message"] = $message;
+		$error_from = "";
+		$error_to = "";
+		$error_subject = "";
+		$error_message = "";
+		$error = false;
+		if($from == "" || !preg_match ("/@/", $from)) {
+			$error_from = "Введите корректный email";
+			$error = true;
+		}
 		
+		if($to == "" || !preg_match ("/@/", $to)) {
+			$error_to = "Введите корректный email";
+			$error = true;
+		}
 		
+		if(strlen($subject) == 0) {
+			$error_subject = "Введите тему сообщения";
+			$error = true
+		}
 		
+		if(strlen($message) == 0) {
+			$error_message = "Введите сообщение";
+			$error = true;
+		}
+		
+		if(!$error) {
+			$subject = "=?utf-8?B?".base64_encode($subject)."?="
+			$headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=utf-8\r\n";
+			mail ($to, $subject, $message, $headers);
+			header ("Location: success.php?send=1");
+			exit;
+		}
 	}
-	se
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,17 +53,23 @@
 	<form name"feedback" action="" method="post">
 		<label>От кого: </label><br />
 		<input type="text" name="from" value="<?=$_SESSION["from"]?>" />
-		<span style="color:red"><?=error_to?></span>
+		<span style="color:red"><?=error_from?></span>
 		<br />
 		
 		<label>Кому: </label><br />
 		<input type="text" name="to" value="<?=$_SESSION["to"]?>" /><br />
+		<span style="color:red"><?=error_to?></span>
+		<br />
 		
 		<label>Тема: </label><br />
 		<input type="text" name="subject" value="<?=$_SESSION["subject"]?>" /><br />
+		<span style="color:red"><?=error_subject?></span>
+		<br />
 		
 		<label>Сообщение: </label><br />
 		<textarea name="message" cols="30" rows="10"><?=$_SESSION["from"]?></textarea><br />
+		<span style="color:red"><?=error_message?></span>
+		<br />
 		
 		<input type="submit" name="send" value="Отправить" />
 	</form>
